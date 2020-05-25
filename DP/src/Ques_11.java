53ad2b918c0f5018f39537bd4762ac232c2d462e
ankur kumar <ankur.kumar@tomtom.com>
2020-05-25 19:07:52 +0530

    dp problems

--- DP/src/Ques_11.java
+++ DP/src/Ques_11.java
@@ -1,25 +1,23 @@
 
 public class Ques_11 {
 
-	static int minCoins(int coins[], int m, int V) {
+	static int minCoins(int coins[], int V) {
 		if (V == 0)
 			return 0;
 
 		int res = Integer.MAX_VALUE;
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < coins.length; i++) {
 			if (coins[i] <= V) {
-				int sub_res = minCoins(coins, m, V - coins[i]);
-				if (sub_res != Integer.MAX_VALUE && sub_res + 1 < res)
-					res = sub_res + 1;
+				int sub_res = minCoins(coins, V - coins[i]);
+				if (sub_res != Integer.MAX_VALUE && sub_res < res)
+					res = sub_res;
 			}
 		}
-		return res;
+		return res+1;
 	}
 
 	public static void main(String args[]) {
 		int coins[] = { 9, 6, 5, 1 };
-		int m = coins.length;
-		int V = 11;
-		System.out.println("Minimum coins required is " + minCoins(coins, m, V));
+		System.out.println("Minimum coins required is " + minCoins(coins, 11));
 	}
 }
