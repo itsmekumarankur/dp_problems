014909154910db29021506a73aa604f4d0cfef00
ronak1114 <you@example.com>
2020-05-26 10:55:20 +0530

    quesn added

--- DP/src/Ques_11.java
+++ DP/src/Ques_11.java
@@ -1,4 +1,8 @@
-
+/*
+ * Find minimum number of coins that make a given value
+Given a value V, if we want to make change for V cents, and we have infinite supply of each of C = { C1, C2, .. , Cm} valued coins, 
+what is the minimum number of coins to make the change?
+ * */
 public class Ques_11 {
 
 	static int minCoins(int coins[], int V) {
