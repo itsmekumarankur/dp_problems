9f036cf7dc4038027c3e23ee365595de10d6987a
ankur kumar <ankur.kumar@tomtom.com>
2020-07-07 11:44:07 +0530

    add

---
+++ GRAPH/src/cb/practise_intro/ques_2_graph_reperesentation/GraphTest.java
@@ -0,0 +1,35 @@
+package temp1.temp1123;
+
+
+class Graph{
+	int[][] graphArray;
+	
+	Graph(int size){
+		graphArray = new int[size][size];
+	}
+	
+	
+	boolean addEdge(int src, int dest, boolean isbidirectional) {
+		
+	}
+	
+	boolean removeEdge(int src, int dest, boolean isbidirectional) {
+		
+	}
+	
+	boolean isEdgePresent(int src, int dest){
+		
+	}
+	
+	
+	void printGraph() {
+		
+	}
+}
+
+
+public class GraphTest{
+	public static void main(String[] args) {
+		
+	}
+}
